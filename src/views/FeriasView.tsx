d9071a1d101903627af84671c611e397bdb5410a
Jesus Condor <jesuscondor837@gmail.com>
2026-01-31 14:18:47 -0500

    Fix FeriasView

--- src/views/FeriasView.tsx
+++ src/views/FeriasView.tsx
@@ -1,4 +1,4 @@
-import styles from "@/app/actividades/Ferias/ferias.module.css";
+import styles from "@/app/actividades/ferias/ferias.module.css";
 
 /* ======================================================
    Tipos
