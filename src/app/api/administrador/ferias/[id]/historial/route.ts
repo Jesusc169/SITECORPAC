299ad8eeb84fa85144cc8b5a0144e66cd392cbf0
Jesus Condor <jesuscondor837@gmail.com>
2026-01-31 20:51:03 -0500

    Fix historial/route.ts

--- src/app/api/administrador/ferias/[id]/historial/route.ts
+++ src/app/api/administrador/ferias/[id]/historial/route.ts
@@ -4,10 +4,11 @@ import { NextResponse } from "next/server";
 export async function GET(_: Request, { params }: any) {
   const feriaId = Number(params.id);
 
-  const historial = await prisma.feria_historial.findMany({
+  const historial = await prisma.feria_historial.findMany({ 
     where: { feria_id: feriaId },
     orderBy: { fecha: "desc" },
   });
 
   return NextResponse.json(historial);
 }
+
