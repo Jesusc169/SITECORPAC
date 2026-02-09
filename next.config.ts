f3ee507a1b822a64eee5f429d36c27ece069fe64
Jesus Condor <jesuscondor837@gmail.com>
2026-02-09 10:41:18 -0500

    Update Next version

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 🔹 Ignora errores de ESLint durante la build de producción
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // 🔹 Otras opciones de Next.js que puedas tener
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 export default nextConfig;
